4fed26937ee288f5594a226a4bf7ce5c1a676d7a
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-25 12:59:25 +0530

    add obj to txt

--- src/main/java/lk/ijse/example/Deserialization/Serialization.java
+++ src/main/java/lk/ijse/example/Deserialization/Serialization.java
@@ -2,6 +2,10 @@ package lk.ijse.example.Deserialization;
 
 import lk.ijse.example.Student;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * @author : savindaJ
  * @date : 12/25/2023
@@ -11,7 +15,23 @@ public class Serialization {
     public static void main(String[] args) {
         Student student = new Student("S001", "savinda", "Matara", 21);
 
+        try {
+            FileOutputStream out = new FileOutputStream("student.txt");
 
+            ObjectOutputStream obj = new ObjectOutputStream(out);
+
+            obj.writeObject(student);
+
+            obj.flush();
+
+            obj.close();
+
+            System.out.println("Succsess !");
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 }
