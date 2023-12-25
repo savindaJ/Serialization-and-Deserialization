ce3edd55741949505c65855db443aa57f31de046
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-25 13:05:38 +0530

    add desirelisation !

--- src/main/java/lk/ijse/example/Deserialization/Deserialization.java
+++ src/main/java/lk/ijse/example/Deserialization/Deserialization.java
@@ -1,5 +1,11 @@
 package lk.ijse.example.Deserialization;
 
+import lk.ijse.example.Student;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 /**
  * @author : savindaJ
  * @date : 12/25/2023
@@ -7,6 +13,15 @@ package lk.ijse.example.Deserialization;
  **/
 public class Deserialization {
     public static void main(String[] args) {
+        try {
+            ObjectInputStream input = new ObjectInputStream(new FileInputStream("G:\\WorkZone\\Serialization\\Serialization-01\\src\\main\\resources\\student.txt"));
 
+            Student student = (Student) input.readObject();
+
+            System.out.println(student);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
