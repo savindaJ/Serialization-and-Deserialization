84189758d74b0f924d4deaea036051176fe48d92
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-25 13:10:01 +0530

    add obj to json

---
+++ src/main/java/lk/ijse/example/Deserialization/JsonToObj.java
@@ -0,0 +1,9 @@
+package lk.ijse.example.Deserialization;
+
+/**
+ * @author : savindaJ
+ * @date : 12/25/2023
+ * @since : 0.1.0
+ **/
+public class JsonToObj {
+}
--- src/main/java/lk/ijse/example/Serialization/ObjToJson.java
+++ src/main/java/lk/ijse/example/Serialization/ObjToJson.java
@@ -1,5 +1,8 @@
 package lk.ijse.example.Serialization;
 
+import com.google.gson.Gson;
+import lk.ijse.example.Student;
+
 /**
  * @author : savindaJ
  * @date : 12/25/2023
@@ -7,6 +10,15 @@ package lk.ijse.example.Serialization;
  **/
 public class ObjToJson {
     public static void main(String[] args) {
+        Student student = new Student("S001", "savinda", "Matara", 21);
+
+        String json = new Gson().toJson(student);
+
+        long l = System.currentTimeMillis();
+
+        System.out.println("Gson Output :"+json+" "+(System.currentTimeMillis()-l+"ms"));
+
+
 
     }
 }
