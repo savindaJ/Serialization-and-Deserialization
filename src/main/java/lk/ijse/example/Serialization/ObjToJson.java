19feb43f14158cc144361659465a73ccef3ee1fb
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-25 13:06:53 +0530

    add dependenceas !

---
+++ src/main/java/lk/ijse/example/Serialization/ObjToJson.java
@@ -0,0 +1,12 @@
+package lk.ijse.example.Serialization;
+
+/**
+ * @author : savindaJ
+ * @date : 12/25/2023
+ * @since : 0.1.0
+ **/
+public class ObjToJson {
+    public static void main(String[] args) {
+
+    }
+}
