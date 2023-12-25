81ccd9c4e5ccde2778315dda07f70683f52c1594
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-25 13:02:52 +0530

    add desirelisation !

---
+++ src/main/java/lk/ijse/example/Deserialization/Deserialization.java
@@ -0,0 +1,12 @@
+package lk.ijse.example.Deserialization;
+
+/**
+ * @author : savindaJ
+ * @date : 12/25/2023
+ * @since : 0.1.0
+ **/
+public class Deserialization {
+    public static void main(String[] args) {
+
+    }
+}
