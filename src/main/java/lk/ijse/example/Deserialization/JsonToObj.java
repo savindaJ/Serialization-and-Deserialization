315fef1d25872e593b497ce08dafc6269849eb59
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-25 13:12:55 +0530

    add deserilisation !

--- src/main/java/lk/ijse/example/Deserialization/JsonToObj.java
+++ src/main/java/lk/ijse/example/Deserialization/JsonToObj.java
@@ -1,5 +1,8 @@
 package lk.ijse.example.Deserialization;
 
+import com.google.gson.Gson;
+import lk.ijse.example.Student;
+
 /**
  * @author : savindaJ
  * @date : 12/25/2023
@@ -7,8 +10,11 @@ package lk.ijse.example.Deserialization;
  **/
 public class JsonToObj {
     public static void main(String[] args) {
+        // json String
         String json = "{\"studentId\":\"S001\",\"studentName\":\"savinda\",\"studentAddress\":\"Matara\",\"studentAge\":21}";
 
-        System.out.println(json);
+        Gson gson = new Gson();
+        Student student = gson.fromJson(json, Student.class);
+        System.out.println(student);
     }
 }
